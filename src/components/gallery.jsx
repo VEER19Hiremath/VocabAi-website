5e7a47aadd7f0cf5020899359ac4ae079c41ac50
mathangisravan7 <mathangisravan7@gmail.com>
2024-09-01 00:01:50 +0530

    updated gallery component

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,8 +1,22 @@
 import { Image } from "./image";
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import '../styles/gallery.css'; // Import the CSS file for the gallery
 
 export const Gallery = (props) => {
+  const galleryInnerRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (galleryInnerRef.current) {
+      galleryInnerRef.current.style.animationPlayState = "paused"; // Pause animation on hover
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (galleryInnerRef.current) {
+      galleryInnerRef.current.style.animationPlayState = "running"; // Resume animation on mouse leave
+    }
+  };
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
@@ -11,12 +25,17 @@ export const Gallery = (props) => {
           <p>This section contains images of Vocab</p>
         </div>
         <div className="gallery-container">
-          <div className="gallery-inner">
+          <div
+            className="gallery-inner"
+            ref={galleryInnerRef}
+          >
             {props.data
               ? props.data.concat(props.data).map((d, i) => (
                   <div
                     key={`${d.title}-${i}`}
                     className="gallery-slide"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
                   >
                     <Image
                       title={d.title}
